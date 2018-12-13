5c839342235172b85f66be24229b4548afac30ab
Erik Osterman <erik@cloudposse.com>
2018-12-13 00:19:15 -0800

    Fix lint warnings

--- main.go
+++ main.go
@@ -36,22 +36,22 @@ func main() {
 	var env = []string{}
 
 	// Prefix used for all normalized environment variables
-	var tfenv_prefix = getEnv("TFENV_PREFIX", "TF_VAR_")
+	var tfenvPrefix = getEnv("TFENV_PREFIX", "TF_VAR_")
 
 	// Whitelist of allowed environment variables. Processed *after* blacklist.
-	var tfenv_whitelist = getEnv("TFENV_WHITELIST", ".*")
+	var tfenvWhitelist = getEnv("TFENV_WHITELIST", ".*")
 
 	// Blacklist of excluded environment variables. Processed *before* whitelist.
-	var tfenv_blacklist = getEnv("TFENV_BLACKLIST", "^(AWS_ACCESS_KEY_ID|AWS_SECRET_ACCESS_KEY)$")
+	var tfenvBlacklist = getEnv("TFENV_BLACKLIST", "^(AWS_ACCESS_KEY_ID|AWS_SECRET_ACCESS_KEY)$")
 
 	// The command that was executed
 	cmd := os.Args[0]
 
-	re_tf_var := regexp.MustCompile("^" + tfenv_prefix)
-	re_trim := regexp.MustCompile("(^_+|_+$)")
-	re_dedupe := regexp.MustCompile("_+")
-	re_whitelist := regexp.MustCompile(tfenv_whitelist)
-	re_blacklist := regexp.MustCompile(tfenv_blacklist)
+	reTfVar := regexp.MustCompile("^" + tfenvPrefix)
+	reTrim := regexp.MustCompile("(^_+|_+$)")
+	reDedupe := regexp.MustCompile("_+")
+	reWhitelist := regexp.MustCompile(tfenvWhitelist)
+	reBlacklist := regexp.MustCompile(tfenvBlacklist)
 
 	for _, e := range os.Environ() {
 		// Preserve the original environment variable
@@ -61,22 +61,22 @@ func main() {
 		pair := strings.Split(e, "=")
 
 		// Process the blacklist for exclusions, then the whitelist for inclusions
-		if !re_blacklist.MatchString(pair[0]) && re_whitelist.MatchString(pair[0]) {
+		if !reBlacklist.MatchString(pair[0]) && reWhitelist.MatchString(pair[0]) {
 			// Strip off TF_VAR_ prefix so we can simplify normalization
-			pair[0] = re_tf_var.ReplaceAllString(pair[0], "")
+			pair[0] = reTfVar.ReplaceAllString(pair[0], "")
 
 			// downcase key
 			pair[0] = strings.ToLower(pair[0])
 
 			// trim leading and trailing underscopres
-			pair[0] = re_trim.ReplaceAllString(pair[0], "")
+			pair[0] = reTrim.ReplaceAllString(pair[0], "")
 
 			// remove consequtive underscopres
-			pair[0] = re_dedupe.ReplaceAllString(pair[0], "_")
+			pair[0] = reDedupe.ReplaceAllString(pair[0], "_")
 
 			// prepend TF_VAR_, if not there already
 			if len(pair[0]) != 0 {
-				pair[0] = tfenv_prefix + pair[0]
+				pair[0] = tfenvPrefix + pair[0]
 				envvar := pair[0] + "=" + pair[1]
 				//fmt.Println(envvar)
 				env = append(env, envvar)
